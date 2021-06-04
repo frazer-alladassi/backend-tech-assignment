2e516847f194682b44815946639b4d0e65707a96
Sedjro <frazer@right-com.com>
2021-06-04 11:49:31 +0100

    Setup project base.

---
+++ src/main/java/com/backend/tech/assignment/BackendTechApplication.java
@@ -0,0 +1,12 @@
+package com.backend.tech.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTechApplication.class, args);
+    }
+}
